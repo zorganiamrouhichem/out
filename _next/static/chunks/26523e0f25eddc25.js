65c5a36114ff6cd2f66080a2e70a493a43edf607
hichemzorgani <hichem.zorgani2003@gmail.com>
2025-09-25 20:27:52 +0100

    first commit

---
+++ _next/static/chunks/26523e0f25eddc25.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/2df266e98d3e8f4d.js",
+  "static/chunks/turbopack-ff1a2df927200443.js"
+])
---
+++ _next/static/chunks/c190a73909abb633.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/2df266e98d3e8f4d.js",
+  "static/chunks/turbopack-784d27b60de37372.js"
+])
